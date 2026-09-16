1dbaa65740c235e79ae1b2f603a3d375f592b928
Krishn Sharma <183143060+krishn-exe@users.noreply.github.com>
2026-09-16 20:35:04 +0530

    feat: create note model, fix timezone

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import Note
 
-# Register your models here.
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'title', 'content', 'updated_at')
+    search_fields = ('title', 'content')
+    list_filter = ('user', 'updated_at')
