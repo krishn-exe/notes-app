d630d26dd05dae8c2c2bdeae8d4261ab6d8cfab3
Krishn Sharma <183143060+krishn-exe@users.noreply.github.com>
2026-09-16 20:59:50 +0530

    feat: add login register urls

--- main/views.py
+++ main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("Welcome to the Notes App")
+
+def register(request):
+    return HttpResponse("Register Page")
+
+def login(request):
+    return HttpResponse("Login Page")
+
+def logout(request):
+    return HttpResponse("Logout Page")
