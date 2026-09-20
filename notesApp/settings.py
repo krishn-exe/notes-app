2933db11030f0b8f1b51358f57f6ac29041920bd
Krishn Sharma <183143060+krishn-exe@users.noreply.github.com>
2026-09-20 11:28:17 +0530

    feat: add rate limiting

--- notesApp/settings.py
+++ notesApp/settings.py
@@ -80,6 +80,14 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '20/minute',
+        'user': '60/minute',
+    }
 }
 
 SPECTACULAR_SETTINGS = {
