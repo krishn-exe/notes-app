1ce3b7aef0c69b221ddb06e4fc5da5dc41306291
Krishn Sharma <183143060+krishn-exe@users.noreply.github.com>
2026-09-20 11:28:17 +0530

    feat: add rate limiting

--- notesApp/settings.py
+++ notesApp/settings.py
@@ -80,6 +80,14 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '20/minute',
+        'user': '60/minute',
+    }
 }
 
 SPECTACULAR_SETTINGS = {
@@ -135,6 +143,16 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'notesApp.wsgi.application'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
 
 
 # Database
